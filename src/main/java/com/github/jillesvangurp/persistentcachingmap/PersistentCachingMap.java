4771ee09553fa038c1e71638abc686ea51ae749c
Jilles van Gurp <jilles@jillesvangurp.com>
2013-02-27 15:46:42 +0100

    use better locking solution to prevent bucket reads overlapping with writes

--- src/main/java/com/github/jillesvangurp/persistentcachingmap/PersistentCachingMap.java
+++ src/main/java/com/github/jillesvangurp/persistentcachingmap/PersistentCachingMap.java
@@ -1,24 +1,24 @@
 /**
-* Copyright (c) 2013, Jilles van Gurp
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * Copyright (c) 2013, Jilles van Gurp
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.jillesvangurp.persistentcachingmap;
 
 import java.io.BufferedReader;
@@ -39,6 +39,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -79,40 +81,54 @@ import com.jillesvangurp.iterables.Processor;
  * @param <Key>
  * @param <Value>
  */
-public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,Value>>,Map<Key,Value>, Closeable {
-    private final PersistentCachingMapCodec<Key,Value> codec;
+public class PersistentCachingMap<Key, Value> implements Iterable<Map.Entry<Key, Value>>, Map<Key, Value>, Closeable {
+    private final PersistentCachingMapCodec<Key, Value> codec;
     private final LoadingCache<Long, Bucket> cache;
     private final Set<Long> bucketIds = new ConcurrentSkipListSet<>();
+    private final IdLock idLock = new IdLock();
+
     private final String dataDir;
 
-    public PersistentCachingMap(String dataDir, PersistentCachingMapCodec<Key,Value> codec, long cacheSize) {
+    public PersistentCachingMap(String dataDir, PersistentCachingMapCodec<Key, Value> codec, long cacheSize) {
         this.codec = codec;
         this.dataDir = dataDir;
         CacheLoader<Long, Bucket> loader = new CacheLoader<Long, Bucket>() {
             @Override
             public Bucket load(Long id) throws Exception {
                 Bucket bucket = new Bucket(id);
-                bucket.read();
+                idLock.acquire(id);
+                try {
+                    bucket.read();
+                } finally {
+                    idLock.release(id);
+                }
                 return bucket;
             }
         };
         cache = CacheBuilder.newBuilder().maximumSize(cacheSize).removalListener(new RemovalListener<Long, Bucket>() {
             @Override
             public void onRemoval(RemovalNotification<Long, Bucket> notification) {
+                Long bucketId = notification.getKey();
                 // make sure changed buckets are written on eviction
-                notification.getValue().write();
+                idLock.acquire(bucketId);
+                try {
+                    notification.getValue().write();
+                } finally {
+                    idLock.release(bucketId);
+                }
             }
         }).build(loader);
         readBucketIds();
     }
 
-    @SuppressWarnings("unchecked") // Map.get is unchecked
+    @SuppressWarnings("unchecked")
+    // Map.get is unchecked
     @Override
     public Value get(Object key) {
         try {
-            long bucketId = codec.bucketId((Key)key);
-            if(bucketIds.contains(bucketId)) {
-                return cache.get(bucketId).get((Key)key);
+            long bucketId = codec.bucketId((Key) key);
+            if (bucketIds.contains(bucketId)) {
+                return cache.get(bucketId).get((Key) key);
             }
         } catch (ExecutionException e) {
             e.printStackTrace();
@@ -125,12 +141,12 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
     public Value put(Key key, Value value) {
         try {
             long bucketId = codec.bucketId(key);
-            synchronized(this) {
-                if(!bucketIds.contains(bucketId)) {
+            synchronized (this) {
+                if (!bucketIds.contains(bucketId)) {
                     bucketIds.add(bucketId);
                 }
                 Bucket bucket = cache.get(bucketId);
-                bucket.put(key,value);
+                bucket.put(key, value);
             }
             return value;
         } catch (ExecutionException e) {
@@ -138,13 +154,14 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
         }
     }
 
-    @SuppressWarnings("unchecked") // Map.remove is unchecked
+    @SuppressWarnings("unchecked")
+    // Map.remove is unchecked
     @Override
     public Value remove(Object key) {
         try {
             long bucketId = codec.bucketId((Key) key);
-            synchronized(this) {
-                if(!bucketIds.contains(bucketId)) {
+            synchronized (this) {
+                if (!bucketIds.contains(bucketId)) {
                     bucketIds.add(bucketId);
                 }
                 Bucket bucket = cache.get(bucketId);
@@ -157,7 +174,7 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
 
     @Override
     public void clear() {
-        for(Long bucketId: bucketIds) {
+        for (Long bucketId : bucketIds) {
             try {
                 cache.get(bucketId).clear();
             } catch (ExecutionException e) {
@@ -167,8 +184,8 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
     }
 
     void writeBucketIds() {
-        if(bucketIds.size() > 0) {
-            File path = new File(dataDir,"bucketIds.gz");
+        if (bucketIds.size() > 0) {
+            File path = new File(dataDir, "bucketIds.gz");
             File dir = path.getParentFile();
             if (!dir.exists()) {
                 if (!dir.mkdirs()) {
@@ -176,9 +193,9 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
                 }
             }
 
-            try(BufferedWriter out = gzipFileWriter(path)) {
-                for(Long id: bucketIds) {
-                    out.write("" + id +"\n");
+            try (BufferedWriter out = gzipFileWriter(path)) {
+                for (Long id : bucketIds) {
+                    out.write("" + id + "\n");
                 }
             } catch (IOException e) {
                 throw new IllegalStateException(e);
@@ -187,10 +204,10 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
     }
 
     void readBucketIds() {
-        File bucketIdFile = new File(dataDir,"bucketIds.gz");
-        if(bucketIdFile.exists()) {
-            try(LineIterable iterable = new LineIterable(gzipFileReader(bucketIdFile))) {
-                for(String line: iterable) {
+        File bucketIdFile = new File(dataDir, "bucketIds.gz");
+        if (bucketIdFile.exists()) {
+            try (LineIterable iterable = new LineIterable(gzipFileReader(bucketIdFile))) {
+                for (String line : iterable) {
                     bucketIds.add(Long.valueOf(line.trim()));
                 }
             } catch (IOException e) {
@@ -205,11 +222,11 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
     }
 
     File bucketPath(long id) {
-        String paddedId = StringUtils.leftPad(""+id, 10, '0');
-        String d1=paddedId.substring(0, 3);
-        String d2=paddedId.substring(3, 6);
+        String paddedId = StringUtils.leftPad("" + id, 10, '0');
+        String d1 = paddedId.substring(0, 3);
+        String d2 = paddedId.substring(3, 6);
 
-        return new File(dataDir, d1+"/"+d2+"/"+paddedId);
+        return new File(dataDir, d1 + "/" + d2 + "/" + paddedId);
     }
 
     @Override
@@ -239,8 +256,8 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
 
     @Override
     public void putAll(Map<? extends Key, ? extends Value> m) {
-        for(Entry<? extends Key, ? extends Value> e:m.entrySet()) {
-            put(e.getKey(),e.getValue());
+        for (Entry<? extends Key, ? extends Value> e : m.entrySet()) {
+            put(e.getKey(), e.getValue());
         }
 
     }
@@ -262,7 +279,7 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
 
     @Override
     public Iterator<java.util.Map.Entry<Key, Value>> iterator() {
-        Processor<Long, Iterable<java.util.Map.Entry<Key, Value>>> processor =  new Processor<Long, Iterable<Entry<Key,Value>>>() {
+        Processor<Long, Iterable<java.util.Map.Entry<Key, Value>>> processor = new Processor<Long, Iterable<Entry<Key, Value>>>() {
 
             @Override
             public Iterable<java.util.Map.Entry<Key, Value>> process(Long bucketId) {
@@ -273,29 +290,60 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
                 }
             }
         };
-        ProcessingIterable<Long, Iterable<Map.Entry<Key, Value>>> iterables = new ProcessingIterable<Long,Iterable<Map.Entry<Key, Value>>>(bucketIds.iterator(), processor );
+        ProcessingIterable<Long, Iterable<Map.Entry<Key, Value>>> iterables = new ProcessingIterable<Long, Iterable<Map.Entry<Key, Value>>>(
+                bucketIds.iterator(), processor);
         return Iterables.compose(iterables).iterator();
     }
 
-
-//    private static BufferedWriter bzipFileWriter(File file) throws IOException {
-//        return new BufferedWriter(new OutputStreamWriter(new BZip2CompressorOutputStream(new FileOutputStream(file)), Charset.forName("utf-8")),64*1024);
-//    }
-//
-//    private static BufferedReader bzipFileReader(File file) throws IOException {
-//        return new BufferedReader(new InputStreamReader(new BZip2CompressorInputStream(new FileInputStream(file)),Charset.forName("utf-8")));
-//    }
+    // private static BufferedWriter bzipFileWriter(File file) throws IOException {
+    // return new BufferedWriter(new OutputStreamWriter(new BZip2CompressorOutputStream(new FileOutputStream(file)),
+    // Charset.forName("utf-8")),64*1024);
+    // }
+    //
+    // private static BufferedReader bzipFileReader(File file) throws IOException {
+    // return new BufferedReader(new InputStreamReader(new BZip2CompressorInputStream(new
+    // FileInputStream(file)),Charset.forName("utf-8")));
+    // }
     private static BufferedWriter gzipFileWriter(File file) throws IOException {
-        return new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(file)), Charset.forName("utf-8")),64*1024);
+        return new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(file)), Charset.forName("utf-8")), 64 * 1024);
     }
 
     private static BufferedReader gzipFileReader(File file) throws IOException {
-        return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)),Charset.forName("utf-8")));
+        return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), Charset.forName("utf-8")));
+    }
+
+    class IdLock {
+        private final Set<Long> ids = new ConcurrentSkipListSet<>();
+        Lock bucketLock = new ReentrantLock();
+
+        public void acquire(Long id) {
+            while(true) {
+                try {
+                    bucketLock.lock();
+                    if(!ids.contains(id)) {
+                        ids.add(id);
+                        return;
+                    }
+                } finally {
+                    bucketLock.unlock();
+                }
+            }
+        }
+
+        public void release(Long id) {
+            try {
+                bucketLock.lock();
+                ids.remove(id);
+            } finally {
+                bucketLock.unlock();
+            }
+
+        }
     }
 
     public class Bucket implements Iterable<Map.Entry<Key, Value>> {
         long id;
-        AtomicBoolean changed=new AtomicBoolean(false);
+        AtomicBoolean changed = new AtomicBoolean(false);
 
         Map<Key, Value> map = new ConcurrentHashMap<>();
 
@@ -322,31 +370,29 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
         }
 
         public void write() {
-            if(changed.get()) {
-                synchronized(cache) {
-                    // prevent somebody reading a partially written bucket by synchronizing on the cache
-                    // need more clever synchronization
-                    File path = bucketPath(id);
-                    if (map.isEmpty() && path.exists()) {
-                        // map is empty, remove the file if it existed and delete the bucket from the bucketIds
-                        if (!path.delete()) {
-                            throw new IllegalStateException("could not delete " + path);
+            if (changed.get()) {
+                // prevent somebody reading a partially written bucket by synchronizing on the cache
+                // need more clever synchronization
+                File path = bucketPath(id);
+                if (map.isEmpty() && path.exists()) {
+                    // map is empty, remove the file if it existed and delete the bucket from the bucketIds
+                    if (!path.delete()) {
+                        throw new IllegalStateException("could not delete " + path);
+                    }
+                    bucketIds.remove(id);
+                } else {
+                    File dir = path.getParentFile();
+                    if (!dir.exists()) {
+                        if (!dir.mkdirs()) {
+                            throw new IllegalStateException("could not create directory " + dir);
                         }
-                        bucketIds.remove(id);
-                    } else {
-                        File dir = path.getParentFile();
-                        if (!dir.exists()) {
-                            if (!dir.mkdirs()) {
-                                throw new IllegalStateException("could not create directory " + dir);
-                            }
+                    }
+                    try (BufferedWriter out = gzipFileWriter(path)) {
+                        for (Entry<Key, Value> e : map.entrySet()) {
+                            out.write("" + codec.serializeKey(e.getKey()) + ";" + codec.serializeValue(e.getValue()) + "\n");
                         }
-                        try (BufferedWriter out = gzipFileWriter(path)) {
-                            for (Entry<Key, Value> e : map.entrySet()) {
-                                out.write("" + codec.serializeKey(e.getKey()) + ";" + codec.serializeValue(e.getValue()) + "\n");
-                            }
-                        } catch (IOException e) {
-                            throw new IllegalStateException("could not write file " + path, e);
-                        }
+                    } catch (IOException e) {
+                        throw new IllegalStateException("could not write file " + path, e);
                     }
                 }
             }
@@ -354,12 +400,12 @@ public class PersistentCachingMap<Key,Value> implements Iterable<Map.Entry<Key,V
 
         public void read() {
             File path = bucketPath(id);
-            if(path.exists()) {
-                try(LineIterable iterable = new LineIterable(gzipFileReader(path))) {
-                    for(String line: iterable) {
+            if (path.exists()) {
+                try (LineIterable iterable = new LineIterable(gzipFileReader(path))) {
+                    for (String line : iterable) {
                         int idx = line.indexOf(';');
-                        Key key = codec.deserializeKey(line.substring(0,idx));
-                        map.put(key, codec.deserializeValue(line.substring(idx+1)));
+                        Key key = codec.deserializeKey(line.substring(0, idx));
+                        map.put(key, codec.deserializeValue(line.substring(idx + 1)));
                     }
                 } catch (IOException e) {
                     throw new IllegalStateException("could not read file " + path, e);
